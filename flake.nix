d791c183c83f716fae0026a4da17fa5e45ba977b
billy4479 <giachi.ellero@gmail.com>
2026-08-18 12:10:10 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          app = pkgs.rustPlatform.buildRustPackage {
+            pname =
+              if builtins.pathExists ./Cargo.toml then
+                (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.name
+              else
+                "app";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+          default = app;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rust-analyzer
+            rustfmt
+            rustc
+            clippy
+          ];
+        };
+      }
+    );
+}
